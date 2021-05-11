b3ffa86f05eb4fb5d2af0093baf7e6e62a06580c
ZAKAR <50302638+smg1945@users.noreply.github.com>
2021-05-11 13:40:22 +0900

    fin

--- data.py
+++ data.py
@@ -1,22 +1,38 @@
 import pandas as pd
 
+conditions = ["confirmed", "deaths", "recovered"]
+
 daily_df = pd.read_csv("data/daily_reports.csv")
-totals_df = daily_df[["Confirmed", "Deaths", "Recovered"]].sum().reset_index(name="count")
+
+totals_df = (
+    daily_df[["Confirmed", "Deaths", "Recovered"]].sum().reset_index(name="count")
+)
 totals_df = totals_df.rename(columns={"index": "condition"})
 
 countries_df = daily_df[["Country_Region", "Confirmed", "Deaths", "Recovered"]]
-countries_df = countries_df.groupby("Country_Region").sum().reset_index()
+countries_df = (
+    countries_df.groupby("Country_Region")
+    .sum()
+    .sort_values(by="Confirmed", ascending=False)
+    .reset_index()
+)
 
-conditions = ["confirmed", "deaths", "recovered"]
+dropdown_options = countries_df.sort_values("Country_Region").reset_index()
+dropdown_options = dropdown_options["Country_Region"]
 
 
 def make_country_df(country):
     def make_df(condition):
-        df = pd.read_csv("data/time_confirmed.csv")
+        df = pd.read_csv(f"data/time_{condition}.csv")
         df = df.loc[df["Country/Region"] == country]
-        df = df.drop(columns=["Province/State", "Country/Region", "Lat", "Long"]).sum().reset_index(name=condition)
+        df = (
+            df.drop(columns=["Province/State", "Country/Region", "Lat", "Long"])
+            .sum()
+            .reset_index(name=condition)
+        )
         df = df.rename(columns={"index": "date"})
         return df
+
     final_df = None
     for condition in conditions:
         condition_df = make_df(condition)
@@ -30,9 +46,14 @@ def make_country_df(country):
 def make_global_df():
     def make_df(condition):
         df = pd.read_csv(f"data/time_{condition}.csv")
-        df = df.drop(["Province/State", "Country/Region", "Lat", "Long"], axis=1).sum().reset_index(name=condition)
+        df = (
+            df.drop(["Province/State", "Country/Region", "Lat", "Long"], axis=1)
+            .sum()
+            .reset_index(name=condition)
+        )
         df = df.rename(columns={"index": "date"})
         return df
+
     final_df = None
     for condition in conditions:
         condition_df = make_df(condition)
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import plotly.express as px
-from data import countries_df
+from data import (
+    countries_df,
+    totals_df,
+    dropdown_options,
+    make_global_df,
+    make_country_df,
+)
 from builders import make_table
 
 stylesheets = [
@@ -11,6 +18,44 @@ stylesheets = [
 ]
 
 app = dash.Dash(__name__, external_stylesheets=stylesheets)
+
+app.title = "Sexy Dashboard"
+
+server = app.server
+
+bubble_map = px.scatter_geo(
+    countries_df,
+    size="Confirmed",
+    projection="equirectangular",
+    hover_name="Country_Region",
+    color="Confirmed",
+    locations="Country_Region",
+    locationmode="country names",
+    size_max=40,
+    title="Confirmed By Country",
+    template="plotly_dark",
+    color_continuous_scale=px.colors.sequential.Oryel,
+    hover_data={
+        "Confirmed": ":,",
+        "Deaths": ":,",
+        "Recovered": ":,",
+        "Country_Region": False,
+    },
+)
+bubble_map.update_layout(
+    margin=dict(l=0, r=0, t=50, b=0), coloraxis_colorbar=dict(xanchor="left", x=0)
+)
+
+bars_graph = px.bar(
+    totals_df,
+    x="condition",
+    hover_data={"count": ":,"},
+    y="count",
+    template="plotly_dark",
+    title="Total Global Cases",
+    labels={"condition": "Condition", "count": "Count", "color": "Condition"},
+)
+bars_graph.update_traces(marker_color=["#e74c3c", "#8e44ad", "#27ae60"])
 
 app.layout = html.Div(
     style={
@@ -24,9 +69,72 @@ app.layout = html.Div(
             style={"textAlign": "center", "paddingTop": "50px", "marginBottom": 100},
             children=[html.H1("Corona Dashboard", style={"fontSize": 40})],
         ),
-        html.Div(children=[html.Div(children=[make_table(countries_df)])]),
+        html.Div(
+            style={
+                "display": "grid",
+                "gap": 50,
+                "gridTemplateColumns": "repeat(4, 1fr)",
+            },
+            children=[
+                html.Div(
+                    style={"grid-column": "span 3"},
+                    children=[dcc.Graph(figure=bubble_map)],
+                ),
+                html.Div(children=[make_table(countries_df)]),
+            ],
+        ),
+        html.Div(
+            style={
+                "display": "grid",
+                "gap": 50,
+                "gridTemplateColumns": "repeat(4, 1fr)",
+            },
+            children=[
+                html.Div(children=[dcc.Graph(figure=bars_graph)]),
+                html.Div(
+                    style={"grid-column": "span 3"},
+                    children=[
+                        dcc.Dropdown(
+                            style={
+                                "width": 320,
+                                "margin": "0 auto",
+                                "color": "#111111",
+                            },
+                            placeholder="Select a Country",
+                            id="country",
+                            options=[
+                                {"label": country, "value": country}
+                                for country in dropdown_options
+                            ],
+                        ),
+                        dcc.Graph(id="country_graph"),
+                    ],
+                ),
+            ],
+        ),
     ],
 )
+
+
+@app.callback(Output("country_graph", "figure"), [Input("country", "value")])
+def update_hello(value):
+    if value:
+        df = make_country_df(value)
+    else:
+        df = make_global_df()
+    fig = px.line(
+        df,
+        x="date",
+        y=["confirmed", "deaths", "recovered"],
+        template="plotly_dark",
+        labels={"value": "Cases", "variable": "Condition", "date": "Date"},
+        hover_data={"value": ":,", "variable": False, "date": False},
+    )
+    fig.update_xaxes(rangeslider_visible=True)
+    fig["data"][0]["line"]["color"] = "#e74c3c"
+    fig["data"][1]["line"]["color"] = "#8e44ad"
+    fig["data"][2]["line"]["color"] = "#27ae60"
+    return fig
 
 
 if __name__ == "__main__":
